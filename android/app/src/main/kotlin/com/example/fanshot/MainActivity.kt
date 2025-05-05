a6c91e6081578a1caf5e907e917a516f83821117
윤서 <Yunse070708@gmail.com>
2025-05-06 01:43:09 +0900

    setting : 프로젝트 세팅(color, font)

---
+++ android/app/src/main/kotlin/com/example/fanshot/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.fanshot
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/app/config/app_color.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  // base
+  static Color whtie = const Color(0xffFFFFFF);
+  static Color black = const Color(0xff000000);
+
+  static Color background = const Color(0xff1B1C1F);
+  static Color yellow = const Color(0xffFFD23E);
+  static Color orange = const Color(0xffF3BF12);
+  static Color green = const Color(0xffC0F21D);
+
+  // gray
+  static Color gray50 = const Color(0xffF1F3F5);
+  static Color gray100 = const Color(0xffD2D4D5);
+  static Color gray200 = const Color(0xffACAFB2);
+  static Color gray300 = const Color(0xff868A8F);
+  static Color gray400 = const Color(0xff6F7379);
+  static Color gray500 = const Color(0xff4B5058);
+  static Color gray600 = const Color(0xff444950);
+  static Color gray700 = const Color(0xff35393E);
+  static Color gray800 = const Color(0xff292C30);
+  static Color gray900 = const Color(0xff202225);
+
+  // primary
+  static Color primarySubtle = const Color(0xffF1F3F5);
+  static Color primaryDefault = const Color(0xff444950);
+  static Color primaryStrong = const Color(0xff35393E);
+  static Color primaryyHeavy = const Color(0xff292C30);
+
+  // secondary
+  static Color secondarySubtle = const Color(0xffFFFFFF);
+  static Color secondaryDefault = const Color(0xffF1F3F5);
+  static Color secondaryStrong = const Color(0xffD2D4D5);
+  static Color secondaryHeavy = const Color(0xffACAFB2);
+
+  // background/state
+  static Color backgroundState = const Color(0xffFDEFEF);
+  static Color backgroundWarning = const Color(0xffFFFCEC);
+  static Color backgroundInfo = const Color(0xffE9F4FE);
+  static Color backgroundSuccess = const Color(0xffEDF9F2);
+
+  // state
+  static Color state = const Color(0xffEA605C);
+  static Color warning = const Color(0xffFAE03E);
+  static Color info = const Color(0xff2391F6);
+  static Color success = const Color(0xff4EC07C);
+}
